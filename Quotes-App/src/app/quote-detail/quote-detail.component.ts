aa4d9216e57aa90f266f4bb061a8bfe8a4e1f25b
kamotho-njiru <samuel.kamotho@student.moringaschool.com>
2021-09-05 22:29:30 +0300

    add an upload-form

--- Quotes-App/src/app/quote-detail/quote-detail.component.ts
+++ Quotes-App/src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import quote blueprint class
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  // property quote to receive data from parent quote component
+  @Input() quote: Quote;
+  @Output() deletequoteevent = new EventEmitter<Quote>();
+  @Output() likequoteevent = new EventEmitter<Quote>();
+  @Output() dislikequoteevent = new EventEmitter<Quote>();
+      
+      //  delete option
+  deletequote(quoteToDelete:Quote){
+    this.deletequoteevent.emit(quoteToDelete);
+  };
+        // like option
+  likeButtonClick(quoteToLike:Quote) {
+    this.likequoteevent.emit(quoteToLike);
+  };
+
+    //  dislike option
+    dislikeButtonClick(quoteToDislike:Quote) {
+      this.dislikequoteevent.emit(quoteToDislike);
+    }
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
